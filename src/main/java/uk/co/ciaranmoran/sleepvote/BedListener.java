7cccb66ff28cb762e23d9c325251a3c448d8b113
iCiaran <iCiaran@users.noreply.github.com>
2020-08-10 19:18:21 +0100

    Add sleep logic

--- src/main/java/uk/co/ciaranmoran/sleepvote/BedListener.java
+++ src/main/java/uk/co/ciaranmoran/sleepvote/BedListener.java
@@ -1,12 +1,27 @@
 package uk.co.ciaranmoran.sleepvote;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Statistic;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 
+import java.util.Collection;
+
 public class BedListener implements Listener {
+
     @EventHandler
     public void onBedEnter(final PlayerBedEnterEvent e) {
-        e.getPlayer().sendMessage("Entered Bed");
+        if(e.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
+            Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
+            long sleeping = onlinePlayers.stream().filter(LivingEntity::isSleeping).count();
+
+            if((float)sleeping/onlinePlayers.size() > 0.5){
+                e.getPlayer().getWorld().setTime(0);
+                onlinePlayers.forEach(p -> p.setStatistic(Statistic.TIME_SINCE_REST, 0));
+            }
+        }
     }
 }
